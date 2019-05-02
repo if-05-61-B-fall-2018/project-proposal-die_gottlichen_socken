591bb2a8018a724cafb1281fb68aa59477debbe0
FabianTischler <fabian.tischler@a1.net>
2019-05-02 14:11:10 +0200

    updated master with easter

---
+++ Bot/Bot/Bot/Bot/Core/Commands/Memes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+
+namespace Bot.Core.Commands
+{
+    public class Memes:ModuleBase<SocketCommandContext>
+    {
+       
+    }
+}
---
+++ Bot/Bot/Bot/Bot/Core/Commands/Roles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+
+namespace Bot.Core.Commands
+{
+    public class Roles : ModuleBase<SocketCommandContext>
+    {
+        [Command("roles")]
+        public async Task Rolesc() 
+        {
+            EmbedBuilder embed = new EmbedBuilder();
+            
+            embed.WithColor(Color.LightGrey);
+            embed.WithAuthor("Roles");
+            embed.WithDescription("**DJ**                  gives the user the rights to skip the Songs \n" +
+                "**Custom Role**                  gives the user a role without bonus rights, where he can decide the name and the color itself \n" +
+                "**Color**                  grants the user a Color which he can decide himself");
+
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+    }
+}
